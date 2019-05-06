23c635c466d0abf277fbfaef7eeca1949d59cd22
user050 <49155137+user050@users.noreply.github.com>
2019-05-06 11:27:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 (function () {
    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-   for (var name in names) {
-      var firstLetter = names[name].charAt(0).toLowerCase();
-       if (firstLetter === 'j') {
-          byeSpeaker.speak(names[name]);
+ for (var i = 0; i < names.length; i++) {
+
+ 
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+ 
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
   } else {
-    helloSpeaker.speak(names[name]);
+    helloSpeaker.speak(names[i]);
   }
 }
+
 
 })();
